1c2826efb90f639626e6558bbb288dc6dff4bd15
Bruno Cestari <brunobcestari@tutamail.com>
2020-03-09 00:00:17 -0300

    Add PUSH method to linked list

--- C/linkedlist.c
+++ C/linkedlist.c
@@ -16,13 +16,34 @@ void print_list(node * head) {
 	}
 }
 
+void push(node ** head, int val) {
+	node * new_head;
+	new_head = (node *) malloc(sizeof(node));
+
+	new_head->val = val;
+	new_head->next = *head;
+	*head = new_head;
+}
+
+
+void add_last(node * head, int val) {
+	node * current = head;
+	while (current->next != NULL) {
+		current = current->next;
+	}
+
+	current->next = (node *) malloc(sizeof(node));
+	current->next->val = val;
+	current->next->next = NULL;
+}
+
+
 int main() {
 	node * test_list = (node *) malloc(sizeof(node));
 	test_list->val = 1;
-	test_list->next = (node *) malloc(sizeof(node));
-	test_list->next->val = 2;
-	test_list->next->next = (node *) malloc(sizeof(node));
-	test_list->next->next->val = 3;
+	push(&test_list, 2);
+	push(&test_list, 3);
+
 	print_list(test_list);
 	return 0;
 }
